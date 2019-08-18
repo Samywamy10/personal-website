80c615561f505eaae307c49dba7e8a93bf6e6e71
Sam Wright <sam.wright@readify.net>
2019-08-18 15:20:16 +1000

    Use history for navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,32 +3,31 @@ import './Navbar.css';
 import Card from "../Card/Card";
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import { History } from "history";
 
-const Navbar: React.FC = () => {
-    const [cardValue, setCardValue] = React.useState(0);
+type NavbarProps = {
+    history: History
+}
 
-    function handleChange(_event: React.ChangeEvent<{}>, newValue: number) {
-      setCardValue(newValue);
-    }
-    
-    useEffect(() => {
-        if(window.location.pathname === "/") {
-            setCardValue(0);
-        } else {
-            setCardValue(1);
+const Navbar: React.FC<NavbarProps> = ({history}) => {
+    const [cardValue, setCardValue] = React.useState('/');
+
+    function handleChange(_event: React.ChangeEvent<{}>, newValue: string) {
+        setCardValue(newValue);
+        if(history) {
+            history.push(newValue);
         }
-    },[])
-
+    }
 
     return (
         <Card className="navbar">
             <Tabs value={cardValue} onChange={handleChange} variant="fullWidth" aria-label="simple tabs example">
-                <Tab component={Link} to={'/'} label="Resume" />
-                <Tab component={Link} to={'/blog'} label="Blog" />
+                <Tab value='/' label="Resume" />
+                <Tab value='/blog' label="Blog" />
             </Tabs>
         </Card>
     )
 }
 
-export default Navbar;
+export default withRouter(Navbar);
